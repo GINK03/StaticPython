f4cd392bbcb7f2d50d38b8e0b480bce14e450e6c
GIMPEI, Kobayashi <angeldust03@gmail.com>
2013-10-15 13:51:36 +0900

    fix a minor bug

--- staticpython-build/mre.py
+++ staticpython-build/mre.py
@@ -6,25 +6,13 @@ import re
 class ___MEMRE:
   def __init__(self):
     self.mem = {}
-    self.mem2 = {}
 __memre = ___MEMRE()
-
-""" パールのレジスタもどきの実装 """
-_e = None
 
 """ メモ化を実装した reの拡張版のmemre """
 def search( regex, line ):
-  global _e
-  """ メモリを溜めすぎた場合は破棄しなくはならない """
-  if len( __memre.mem2.keys() ) > 100:
-    __memre.mem2 = {} # dispiose memory 
-
   if not __memre.mem.get( regex ):
     __memre.mem.setdefault( regex, re.compile(regex) )
-  if not __memre.mem2.get( line ):
-    __memre.mem2.setdefault( line, __memre.mem[regex].search(line) )
   __m = __memre.mem2[line]
-  _e  = __m.groups()
   return __m
 
 ''' here is test '''
